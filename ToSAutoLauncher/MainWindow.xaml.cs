7d4fb3767744a3c1ab46ddb5a85ecf00c7283dd2
kagasu <admin@kagasu.net>
2016-08-24 05:50:30 +0900

    fix login

--- ToSAutoLauncher/MainWindow.xaml.cs
+++ ToSAutoLauncher/MainWindow.xaml.cs
@@ -28,18 +28,20 @@ namespace ToSAutoLauncher
     {
         private class NGMModuleInfo
         {
-            public string host { get; set; }
-            public string crc { get; set; }
+            public string Host { get; set; }
+            public string Crc { get; set; }
         }
 
         private class UserInfo
         {
-            public string id{ get; set; }
-            public string password{ get; set; }
+            public string Id{ get; set; }
+            public string Password{ get; set; }
+            public bool UseOtp { get; set; }
         }
         
         public MainWindow()
         {
+            ServicePointManager.Expect100Continue = false;
             InitializeComponent();
 
             LoadUserInfo();
@@ -61,12 +63,13 @@ namespace ToSAutoLauncher
             }
         }
 
-        private void SaveUserInfo()
+        private void SaveUserInfo(bool useOtp)
         {
             var userInfo = new UserInfo
             {
-                id = textBoxID.Text,
-                password = textBoxPassword.Password
+                Id = textBoxID.Text,
+                Password = textBoxPassword.Password,
+                UseOtp = useOtp
             };
 
             File.WriteAllText("userInfo.json", DynamicJson.Serialize(userInfo));
@@ -78,8 +81,8 @@ namespace ToSAutoLauncher
             {
                 var json = (UserInfo)DynamicJson.Parse(File.ReadAllText("userInfo.json"));
 
-                textBoxID.Text = json.id;
-                textBoxPassword.Password = json.password;
+                textBoxID.Text = json.Id;
+                textBoxPassword.Password = json.Password;
             }
         }
 
@@ -87,23 +90,11 @@ namespace ToSAutoLauncher
         {
             if (textBoxOTP.Text.Length == 6)
             {
-                SaveUserInfo();
+                SaveUserInfo(true);
                 progressbar1.IsIndeterminate = true;
                 Task.Run( () =>
                 {
-                    var npp = Login();
-                    if (npp == null)
-                    {
-                        Dispatcher.Invoke(() => this.ShowMessageAsync("Error", "ログインに失敗しました"));
-                        textBoxOTP.Dispatcher.Invoke(() => textBoxOTP.Text = "");
-                        progressbar1.Dispatcher.Invoke(() => progressbar1.IsIndeterminate = false);
-                        return;
-                    }
-                    var moduleInfo = GetNGMModuleInfo();
-                        
-                    Process.Start($"ngmj://launch/-dll:{moduleInfo.host}:{moduleInfo.crc} -locale:JP -mode:launch -game:16818186:0 -token:'{npp}' -passarg:''");
-                    progressbar1.Dispatcher.Invoke( () => progressbar1.IsIndeterminate = false);
-                    textBoxOTP.Dispatcher.Invoke(() => textBoxOTP.Text = "");
+                    Login();
                 });
             }
         }
@@ -116,22 +107,22 @@ namespace ToSAutoLauncher
             var host = reg.Matches(str)[0].Groups[1].Value;
             var crc = reg.Matches(str)[0].Groups[2].Value;
 
-            return new NGMModuleInfo() { host = host, crc = crc };
+            return new NGMModuleInfo() { Host = host, Crc = crc };
         }
         
-        private string Login()
+        private string NexonLogin()
         {
             var wc = new WebClientEx();
-            var str = wc.DownloadString("http://www.nexon.co.jp/login/index.aspx");
+            var str = wc.DownloadString("https://login.nexon.co.jp/login/");
 
             var entmReg = new Regex("name=\"entm\" value='(.*?)'");
             var idReg = new Regex("value='' id='(.*?)'");
             var passwordReg = new Regex("type=\"password\" id='(.*?)'");
             
-            wc.Headers.Set("Referer", "http://www.nexon.co.jp/login/index.aspx");
+            wc.Headers.Set("Referer", "https://login.nexon.co.jp/login/");
             wc.Headers.Set("User-Agent", "Mozilla");
 
-            wc.UploadValues("https://www.nexon.co.jp/login/login_process1.aspx", new NameValueCollection()
+            wc.UploadValues("https://login.nexon.co.jp/login/login_process1.aspx ", new NameValueCollection()
             {
                 { "entm", entmReg.Matches(str)[0].Groups[1].Value },
                 { idReg.Matches(str)[0].Groups[1].Value, textBoxID.Dispatcher.Invoke(() => textBoxID.Text) },
@@ -141,13 +132,40 @@ namespace ToSAutoLauncher
                 { "otp", "" }
             });
 
-            if (wc.CookieContainer.Count != 0)
+            if (wc.CookieContainer.GetCookieHeader(new Uri("http://nexon.co.jp")).Count() != 0)
             {
                 var npp = wc.CookieContainer.GetCookieHeader(new Uri("http://nexon.co.jp")).Split('=')[1];
                 return npp;
             }
 
             return null;
+        }
+
+        private void Login()
+        {
+            var npp = NexonLogin();
+            if (npp == null)
+            {
+                Dispatcher.Invoke(() => this.ShowMessageAsync("Error", "ログインに失敗しました"));
+                textBoxOTP.Dispatcher.Invoke(() => textBoxOTP.Text = "");
+                progressbar1.Dispatcher.Invoke(() => progressbar1.IsIndeterminate = false);
+                return;
+            }
+            var moduleInfo = GetNGMModuleInfo();
+
+            Process.Start($"ngmj://launch/-dll:{moduleInfo.Host}:{moduleInfo.Crc} -locale:JP -mode:launch -game:16818186:0 -token:'{npp}' -passarg:''");
+            progressbar1.Dispatcher.Invoke(() => progressbar1.IsIndeterminate = false);
+            textBoxOTP.Dispatcher.Invoke(() => textBoxOTP.Text = "");
+        }
+
+        private void buttonOtp_Click(object sender, RoutedEventArgs e)
+        {
+            SaveUserInfo(false);
+            progressbar1.IsIndeterminate = true;
+            Task.Run(() =>
+            {
+                Login();
+            });
         }
     }
 }
